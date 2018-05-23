287bbf96f6e2ff1c0792a200eaecd014045ffc76
Courtney Edwards <cedwards@hotels.com>
2018-05-23 15:03:16 +0100

    keep packages in sync

--- waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/metastore/ReconnectingTunnellingMetaStoreClientFactory.java
+++ waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/metastore/ReconnectingTunnellingMetaStoreClientFactory.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016-2018 Expedia Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hotels.bdp.waggledance.metastore;
 
 import java.net.URI;
@@ -15,8 +30,8 @@ import com.google.common.base.Joiner;
 import com.hotels.bdp.waggledance.api.model.AbstractMetaStore;
 import com.hotels.bdp.waggledance.api.model.MetastoreTunnel;
 import com.hotels.hcommon.hive.metastore.client.HiveMetaStoreClientSupplier;
-import com.hotels.hcommon.hive.metastore.client.ReconnectingMetaStoreClientFactory;
-import com.hotels.hcommon.hive.metastore.client.TunnellingMetaStoreClientSupplierBuilder;
+import com.hotels.hcommon.hive.metastore.client.reconnecting.ReconnectingMetaStoreClientFactory;
+import com.hotels.hcommon.hive.metastore.client.tunnelling.TunnellingMetaStoreClientSupplierBuilder;
 import com.hotels.hcommon.hive.metastore.client.api.CloseableMetaStoreClient;
 import com.hotels.hcommon.hive.metastore.client.api.MetaStoreClientFactory;
 import com.hotels.hcommon.hive.metastore.conf.HiveConfFactory;
