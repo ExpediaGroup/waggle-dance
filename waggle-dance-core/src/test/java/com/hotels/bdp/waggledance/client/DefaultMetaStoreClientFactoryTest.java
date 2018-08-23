e0f0857ecd0810021d27da16afeb0dc21cca5a54
Andreea Paduraru <t-apaduraru@hotels.com>
2018-08-23 10:25:37 +0100

    Added tests for DefaultMetaStoreClientFactory

--- waggle-dance-core/src/test/java/com/hotels/bdp/waggledance/client/DefaultMetaStoreClientFactoryTest.java
+++ waggle-dance-core/src/test/java/com/hotels/bdp/waggledance/client/DefaultMetaStoreClientFactoryTest.java
@@ -2,11 +2,14 @@ package com.hotels.bdp.waggledance.client;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.apache.hadoop.hive.metastore.api.ThriftHiveMetastore.Iface;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransportException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -54,6 +57,14 @@ public class DefaultMetaStoreClientFactoryTest {
   }
 
   @Test
+  public void closeNullBase() throws Exception {
+    CloseableThriftHiveMetastoreIface iface = factory.newInstance("name", RECONNECTION_RETRIES, null);
+
+    iface.close();
+    verify(base, never()).close();
+  }
+
+  @Test
   public void defaultMethodCall() throws Exception {
     when(base.getClient()).thenReturn(client);
     when(client.getName()).thenReturn("ourName");
@@ -64,4 +75,45 @@ public class DefaultMetaStoreClientFactoryTest {
     assertThat(result, is("ourName"));
   }
 
+  @Test
+  public void defaultMethodCallThrowsTransportExceptionRetries() throws TException {
+    when(base.getClient()).thenReturn(client);
+    when(client.getName()).thenThrow(new TTransportException()).thenReturn("ourName");
+
+    CloseableThriftHiveMetastoreIface iface = factory.newInstance("name", RECONNECTION_RETRIES, base);
+
+    String result = iface.getName();
+    assertThat(result, is("ourName"));
+    verify(base).reconnect();
+  }
+
+  @Test(expected = MetastoreUnavailableException.class)
+  public void defaultMethodCallThrowsTransportExceptionShouldntRetry() throws TException {
+    when(base.getClient()).thenReturn(client);
+    doThrow(new TTransportException()).when(client).shutdown();
+
+    CloseableThriftHiveMetastoreIface iface = factory.newInstance("name", RECONNECTION_RETRIES, base);
+
+    iface.shutdown();
+  }
+
+  @Test(expected = MetastoreUnavailableException.class)
+  public void defaultMethodCallThrowsTransportExceptionNoRetriesLeft() throws TException {
+    when(base.getClient()).thenReturn(client);
+    when(client.getName()).thenThrow(new TTransportException());
+
+    CloseableThriftHiveMetastoreIface iface = factory.newInstance("name", 0, base);
+
+    iface.getName();
+  }
+
+  @Test(expected = TException.class)
+  public void defaultMethodCallThrowsRealException() throws TException {
+    when(base.getClient()).thenReturn(client);
+    when(client.getName()).thenThrow(new TException());
+
+    CloseableThriftHiveMetastoreIface iface = factory.newInstance("name", RECONNECTION_RETRIES, base);
+
+    iface.getName();
+  }
 }
