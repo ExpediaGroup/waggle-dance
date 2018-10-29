de5f8d515dd1f1887364145796b7065a8f1a5c10
Ana Jalba <ana.jalba13@gmail.com>
2018-10-29 13:48:37 +0000

    Update method name and pom

--- waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/client/tunnelling/TunnelableFactorySupplier.java
+++ waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/client/tunnelling/TunnelableFactorySupplier.java
@@ -44,7 +44,7 @@ public class TunnelableFactorySupplier {
         .withKnownHosts(metastoreTunnel.getKnownHosts())
         .withLocalhost(metastoreTunnel.getLocalhost())
         .withPrivateKeys(metastoreTunnel.getPrivateKeys())
-        .withStrictHostKeyChecking(metastoreTunnel.isStrictHostKeyChecking())
+        .withStrictHostKeyChecking(metastoreTunnel.getIsStrictHostKeyChecking())
         .build();
   }
 }
