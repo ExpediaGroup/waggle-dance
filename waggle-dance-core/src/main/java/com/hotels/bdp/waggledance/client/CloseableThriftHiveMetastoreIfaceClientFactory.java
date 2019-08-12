ef2efe2eef3abc4ff98dde58d66b0175115820f8
Ana Jalba <ana.jalba13@gmail.com>
2019-08-12 11:13:30 +0100

    Add null check

--- waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/client/CloseableThriftHiveMetastoreIfaceClientFactory.java
+++ waggle-dance-core/src/main/java/com/hotels/bdp/waggledance/client/CloseableThriftHiveMetastoreIfaceClientFactory.java
@@ -63,7 +63,9 @@ public class CloseableThriftHiveMetastoreIfaceClientFactory {
     }
     Map<String, String> properties = new HashMap<>();
     properties.put(ConfVars.METASTOREURIS.varname, uris);
-    properties.putAll(waggleDanceConfiguration.getConfigurationProperties());
+    if (waggleDanceConfiguration.getConfigurationProperties() != null) {
+      properties.putAll(waggleDanceConfiguration.getConfigurationProperties());
+    }
     HiveConfFactory confFactory = new HiveConfFactory(Collections.emptyList(), properties);
     return defaultMetaStoreClientFactory
         .newInstance(confFactory.newInstance(), "waggledance-" + name, DEFAULT_CLIENT_FACTORY_RECONNECTION_RETRY,
